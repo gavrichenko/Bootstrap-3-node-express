4b7d7ab400badc9cfbd4777aee3e21b61e474f20
ia.gavrichenko <ia.gavrichenko@tensor.ru>
2017-09-08 12:19:25 +0300

    Init

---
+++ static/js/myJS.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$("#myCarousel").carousel({
+			 interval : 5000
+	});
+	$('[data-toggle="tooltip"]').tooltip()
+	$('[data-toggle="popover"]').popover()
+	// $(function(){
+		
+	// });
+	
+	    // $(".run").click(function(){
+ 
+        // $("#box").animate({opacity: "0.1", left: "+=400"}, 1200)
+        // .animate({opacity: "0.4", top: "+=160", height: "20", width: "20"}, "slow")
+        // .animate({opacity: "1", left: "0", height: "100", width: "100"}, "slow")
+        // .animate({top: "0"}, "fast")
+        // .slideUp()
+        // .slideDown("slow")
+        // return false;
+    // }); 
+})
